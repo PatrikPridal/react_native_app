bff525f63a1bbd98e90660e3f3c29aad7550ea4f
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-04-02 15:04:01 +0200

    FocusedStatusBar

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,15 @@
-import { Text } from "react-native"
+import { useState } from 'react';
+import { View, SafeArea, FlatList, Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
   )
 }
 
