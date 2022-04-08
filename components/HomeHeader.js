ae28af68b3972abd77fb63645f80fd34273784e7
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-04-08 11:18:49 +0200

    logo + logged person plus blue badge

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,6 +1,6 @@
 import { View, Text, Image, TextInput } from 'react-native';
 
-import { COLORTS, FONTS, SIZES, assets } from '../constants';
+import { COLORS, FONTS, SIZES, assets } from '../constants';
 
 const HomeHeader = () => {
   return (
@@ -8,8 +8,29 @@ const HomeHeader = () => {
       backgroundColor: COLORS.primary,
       padding: SIZES.font
     }}>
-      <View>
-        
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        <Image 
+          source={assets.logo}
+          resize="contain"
+          style={{ width: 90, height: 25 }}
+        />
+
+        <View style={{ width: 45, height: 45 }}>
+          <Image 
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image 
+            source={assets.badge}
+            resizeMode="contain"
+            style={{ position: "absolute", width: 15, height: 15, bottom: 0, right: 0 }}
+          />
+        </View>
       </View>
     </View>
   )
