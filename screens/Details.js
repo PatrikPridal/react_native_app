cdab74e0c1b713706ea93ab0bb68085044565f62
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-04-11 12:47:44 +0200

    button for going back from details

--- screens/Details.js
+++ screens/Details.js
@@ -8,6 +8,12 @@ const DetailsHeader = ({ data, navigation }) => (
   <View style={{ width: '100%', height: 373 }}>
     <Image 
       source={data.image}
+      resizeMode="cover"
+      style={{ width: '100%', height: '100%' }}
+    />
+
+    <CircleButton 
+      imgUrl={assets.left}
     />
   </View>
 )
