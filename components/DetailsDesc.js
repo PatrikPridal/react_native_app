6e1b8a0f56d7beb5dc172a262ab81dfadd483c91
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-04-14 15:48:44 +0200

    detailsdescription

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import { useState } from 'react';
 
-const DetailsDesc = () => {
+import { EthPrice, NFTTitle } from './SubInfo';
+import { COLORS, SIZES, FONTS } from '../constants';
+
+const DetailsDesc = ({ data }) => {
   return (
-    <View>
-      <Text>DetailsDesc</Text>
-    </View>
+    <>
+      <View>
+        
+      </View>
+    </>
   )
 }
 
