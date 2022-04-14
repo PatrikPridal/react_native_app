394d05b66d019297e5457acd53262ba66c69e060
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-04-14 16:09:54 +0200

    button and function for read more and read less

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -6,6 +6,8 @@ import { COLORS, SIZES, FONTS } from '../constants';
 
 const DetailsDesc = ({ data }) => {
   const [text, setText] = useState(data.description.slice(0, 100));
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <>
       <View style={{
@@ -37,7 +39,27 @@ const DetailsDesc = ({ data }) => {
           fontFamily: FONTS.regular,
           color: COLORS.secondary,
           lineHeight: SIZES.large
-        }}>{text}</Text>
+        }}>
+          {text}        
+          {!readMore && '...'}
+          <Text style={{
+          fontSize: SIZES.small,
+          fontFamily: FONTS.semiBold,
+          color: COLORS.primary,
+        }}
+        onPress={() => {
+          if(!readMore) {
+            setText(data.description);
+            setReadMore(true);
+          } else {
+            setText(data.description.slice(0, 100));
+            setReadMore(false);
+          }
+        }}
+        >
+            {readMore ? ' Show Less' : " Read More"}
+          </Text>
+        </Text>
         </View>
 
       </View>
